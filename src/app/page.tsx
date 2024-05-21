e6415d2076185a5dabaf359c0805c3d246bf31dc
Nikita <nikita@gmail.com>
2024-05-21 18:55:52 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
     return (
-        <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+        <main className="min-h-screen bg-black/[0.96] antialiased ">
             <HeroSection/>
+            <FeaturedCourses />
+            <WhyChooseUs/>
+            <MusicSchoolTestimonials/>
         </main>
     );
 }
